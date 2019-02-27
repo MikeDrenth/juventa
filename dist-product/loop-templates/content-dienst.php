d88050f218d5e267316b0e39730cfaec67d8fdfe
MikeDrenth <mdrenth.1990@gmail.com>
2019-02-27 13:22:09 +0100

    [TASK] making dynamic blocks for dienst page template

--- dist-product/loop-templates/content-dienst.php
+++ dist-product/loop-templates/content-dienst.php
@@ -28,22 +28,26 @@
     </div>
 </div>
 
-<?php if( have_rows('diensten', 12) ): ?>
+<?php if( have_rows('overige_diensten') ): ?>
 	<div id="row2">
         <div id="diensten" class="fadeAnimation container">
-            <div class="row justify-content-center title-row">
-                <div class="col-12 col-md-10">
-                    <?php the_field('diensten_tekst', 57); ?>
-                </div>
+            <div class="row justify-content-center title-row text-center">
+                <?php while ( have_rows('overige_diensten') ) : the_row();  ?>
+                    <?php if( get_row_layout() == 'intro' ): ?>
+                    <div class="col-12 col-md-10">
+                        <?php the_sub_field('tekst'); ?>
+                    </div>
+                    <?php endif; ?>
+                <?php endwhile;?>
             </div>
             <div class="row justify-content-center items">
                 <div class="col-12">
-                    <div class="row content-row">
-                        <?php while ( have_rows('diensten', 12) ) : the_row();  ?>
-                            <?php if( get_row_layout() == 'diensten' ): ?>
+                    <div class="row content-row justify-content-center">
+                        <?php while ( have_rows('overige_diensten') ) : the_row();  ?>
+                            <?php if( get_row_layout() == 'dienst' ): ?>
                                 <div class="col-12 col-sm-4">
                                     <div class="item">
-                                        <?php the_sub_field('tekst', 12); ?>
+                                        <?php the_sub_field('omschrijving'); ?>
                                     </div>
                                 </div>
                             <?php endif; ?>
--- footer.php
+++ footer.php
@@ -12,14 +12,21 @@
 <div id="contact" class="footer-wrapper d-flex flex-column justify-content-center">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-4">
-                <?php dynamic_sidebar( 'footer-bottom-links' ); ?>
+            <div class="col-12 col-lg-8">
+                <?php echo do_shortcode('[wp_mapbox_gl_js map_id="146"]'); ?>
             </div>
-            <div class="col-12 col-md-4">
-                <?php dynamic_sidebar( 'footer-bottom-center' ); ?>
-            </div>
-            <div class="col-12 col-md-4">
-                <?php dynamic_sidebar( 'footer-bottom-right' ); ?>
+            <div class="col-12 col-lg-4">
+                <div class="row">
+                    <div class="col-12 col-md-4 col-lg-12">
+                        <?php dynamic_sidebar( 'footer-bottom-links' ); ?>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-12">
+                        <?php dynamic_sidebar( 'footer-bottom-center' ); ?>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-12">
+                        <?php dynamic_sidebar( 'footer-bottom-right' ); ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
